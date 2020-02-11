d5c76d99564482362bb57f7a316489cec2473a57
Prudhvi Ram <prudhviram.maddina@bpmlinks.com>
2020-02-11 19:22:50 +0530

    test case for rendering app without crashing

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,17 @@
-test('Sample test adding positive numbers is not zero', () => {
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+
+test('Sample test case: adding positive numbers is not zero', () => {
     for (let a = 1; a < 10; a++) {
       for (let b = 1; b < 10; b++) {
           expect(a + b).not.toBe(0);
       }
     }
   });
+  
+test('App test case: renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
